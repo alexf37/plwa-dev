2b2f30a26354f68fc3bd57bacd73e7b13371314a
Alex Foster <xrk4np@virginia.edu>
2023-09-09 15:47:03 -0400

    format with prettier

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <main className="h-full bg-slate-800 text-slate-50 grid place-content-center">
+      <main className="grid h-full place-content-center bg-slate-800 text-slate-50">
         <div className="mx-auto flex h-4/5 w-full max-w-2xl flex-col items-start justify-center text-left">
           <h1 className="mb-2 text-5xl font-bold tracking-wide text-slate-400">
             Welcome!
@@ -9,15 +9,33 @@ function App() {
           <h2 className="mb-4 text-3xl font-semibold tracking-wide">
             Alex Foster's CS4640 Site
           </h2>
-          <p className="tracking-wide text-slate-200 mb-2">
-            I made this site with Vite, React, Typescript, Tailwind, PHP, and some plain old HTML+CSS. The typeface I'm using is called <a className="underline underline-offset-4 hover:text-white" target="_blank" href="https://rsms.me/inter/">Inter,</a> one of my favourite fonts. 
+          <p className="mb-2 tracking-wide text-slate-200">
+            I made this site with Vite, React, Typescript, Tailwind, PHP, and
+            some plain old HTML+CSS. The typeface I'm using is called{" "}
+            <a
+              className="underline underline-offset-4 hover:text-white"
+              target="_blank"
+              href="https://rsms.me/inter/"
+            >
+              Inter,
+            </a>{" "}
+            one of my favourite fonts.
           </p>
           <p className="tracking-wide text-slate-200">
-             I'm not sure what to do with this site just yet but hopefully I can think of something cool to park here alongside my assignments. Speaking of which, maybe I should add a menu so you can actually get to them...
+            I'm not sure what to do with this site just yet but hopefully I can
+            think of something cool to park here alongside my assignments.
+            Speaking of which, maybe I should add a menu so you can actually get
+            to them...
           </p>
-          <div className="mt-4 flex gap-2 text-slate-300 font-semibold text-lg">
-        <a className="hover:underline underline-offset-4" href="/xrk4np/hw0/homework0.html">HW 0 - Publishing</a><span className="arrow-stretch">&gt;</span> 
-      </div>
+          <div className="mt-4 flex gap-2 text-lg font-semibold text-slate-300">
+            <a
+              className="underline-offset-4 hover:underline"
+              href="/xrk4np/hw0/homework0.html"
+            >
+              HW 0 - Publishing
+            </a>
+            <span className="arrow-stretch">&gt;</span>
+          </div>
         </div>
       </main>
     </>
