34ea4f111f9d7589bb13a104ce5db4b61e6f253a
Alex Foster <xrk4np@virginia.edu>
2023-10-06 15:42:00 -0400

    implement map behaviour

--- web/src/app/App.tsx
+++ web/src/app/App.tsx
@@ -1,22 +1,53 @@
-import { useEffect, useRef, useState } from "react";
+import { useDeferredValue, useEffect, useRef, useState } from "react";
 import { type MapRef } from "react-map-gl";
 import { Map } from "./Map";
 
+type Location = {
+  latitude: number;
+  longitude: number;
+};
+
+const INITIAL_VIEWPORT = {
+  latitude: 38.035629,
+  longitude: -78.503403,
+  zoom: 14,
+};
+
 export default function App() {
   const mapRef = useRef<MapRef>(null);
-  const [mapViewport, setMapViewport] = useState({
-    latitude: 51.5074,
-    longitude: 0.1278,
-    zoom: 10,
-  });
+  const [mapViewport, setMapViewport] = useState(INITIAL_VIEWPORT);
+  const deferredViewport = useDeferredValue(mapViewport);
   useEffect(() => {
     mapRef.current?.flyTo({
-      center: [mapViewport.longitude, mapViewport.latitude],
+      center: [INITIAL_VIEWPORT.longitude, INITIAL_VIEWPORT.latitude],
     });
-  }, [mapViewport.longitude, mapViewport.latitude]);
+  }, []);
+
+  const handleFlyToOnClick = ({ latitude, longitude }: Location) => {
+    mapRef.current?.flyTo({
+      center: [longitude, latitude],
+    });
+  };
+
   return (
-    <div className="relative h-screen w-screen">
-      <Map ref={mapRef} {...mapViewport}></Map>
+    <div className="relative h-full">
+      <Map
+        ref={mapRef}
+        {...deferredViewport}
+        onMove={({ viewState: { latitude, longitude, zoom } }) => {
+          setMapViewport({ latitude, longitude, zoom });
+        }}
+        optimizeForTerrain={true}
+        onClick={(e) => {
+          e.preventDefault();
+          const { lat: latitude, lng: longitude } = e.lngLat;
+          handleFlyToOnClick({ latitude, longitude });
+        }}
+      ></Map>
+      <div className="absolute text-xl">
+        <h2>{deferredViewport.latitude}</h2>
+        <h2>{mapRef?.current?.getCenter().lat}</h2>
+      </div>
     </div>
   );
 }
