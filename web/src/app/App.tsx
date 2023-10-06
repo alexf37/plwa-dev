e6d0bb2f1f0267c03e189077c8ef2793121e076b
Alex Foster <xrk4np@virginia.edu>
2023-10-06 12:51:26 -0400

    get map working

--- web/src/app/App.tsx
+++ web/src/app/App.tsx
@@ -1,9 +1,22 @@
-function App() {
+import { useEffect, useRef, useState } from "react";
+import { type MapRef } from "react-map-gl";
+import { Map } from "./Map";
+
+export default function App() {
+  const mapRef = useRef<MapRef>(null);
+  const [mapViewport, setMapViewport] = useState({
+    latitude: 51.5074,
+    longitude: 0.1278,
+    zoom: 10,
+  });
+  useEffect(() => {
+    mapRef.current?.flyTo({
+      center: [mapViewport.longitude, mapViewport.latitude],
+    });
+  }, [mapViewport.longitude, mapViewport.latitude]);
   return (
-    <>
-      <div></div>
-    </>
+    <div className="relative h-screen w-screen">
+      <Map ref={mapRef} {...mapViewport}></Map>
+    </div>
   );
 }
-
-export default App;
