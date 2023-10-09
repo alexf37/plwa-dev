a39d65180252091847bf218b1f372a58a76fb8e8
Alex Foster <xrk4np@virginia.edu>
2023-10-08 22:56:18 -0400

    useTitle on all pages except spot

--- src/NewPost.tsx
+++ src/NewPost.tsx
@@ -1,6 +1,8 @@
 import { router } from "./routes";
+import { useTitle } from "./hooks/useTitle"
 
 export function NewPost() {
+  useTitle("New Post | SpotChat");
   return (
     <div className="pointer-events-auto flex h-min w-96 flex-col rounded-3xl border border-slate-200 bg-white p-8 shadow-lg">
       <div className="flex items-center justify-between border-b border-slate-200 pb-6">
