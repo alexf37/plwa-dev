48dff1b170679e7a22180133ab0cbb262f11d7d5
Alex Foster <xrk4np@virginia.edu>
2023-10-12 17:40:13 -0400

    add input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { twMerge } from "tailwind-merge";
+
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <input
+        ref={ref}
+        className={twMerge(
+          "border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          className,
+        )}
+        {...props}
+      />
+    );
+  },
+);
+Input.displayName = "Input";
+export { Input };
