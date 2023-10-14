ebdf87743ae2c474869308bb772e64d4afea0dde
Alex Foster <xrk4np@virginia.edu>
2023-10-14 17:11:29 -0400

    finish hw4 php assignment implementation

--- public/hw4/homework4.php
+++ public/hw4/homework4.php
@@ -13,8 +13,8 @@ function calculateGrade(array $scores, bool $drop)
 
     $score_total = 0;
     $max_total = 0;
-    $lowest_percentage = INF;
-    $lowest_max = 0;
+    $lowest_percentage = PHP_INT_MAX;
+    $lowest_max = 1;
 
     foreach ($scores as $score) {
         $score_total += $score["score"];
@@ -31,6 +31,10 @@ function calculateGrade(array $scores, bool $drop)
         $max_total -= $lowest_max;
     }
 
+    if ($max_total == 0) {
+        return 0;
+    }
+
     return round(100 * $score_total / $max_total, 3);
 }
 
@@ -39,7 +43,19 @@ function gridCorners($width, $height)
     $last = $width * $height;
     $left = [1, 2, $height, $height - 1, $height + 1, $height * 2];
     $right = [$last, $last - 1, $last - $height, $last - $height + 1, $last - $height + 2, $last - 2 * $height + 1];
+    $edges = [];
+    if ($width == 1 || $height == 1) {
+        for ($i = 1; $i <= $width * $height; $i++) {
+            array_push($edges, $i);
+        }
+        return join(", ", $edges);
+    }
     $all = array_merge($left, $right);
+    foreach ($all as $i => $value) {
+        if ($value < 1 || $value > $last) {
+            unset($all[$i]);
+        }
+    }
     $all = array_unique($all);
     sort($all);
     // still need to account for m<2 and n<2
@@ -48,28 +64,32 @@ function gridCorners($width, $height)
 
 function combineShoppingLists(...$lists)
 {
+    if (!$lists) return [];
     $merged = [];
     foreach ($lists as $list) {
+        if (!isset($list['user']) || !isset($list['list'])) continue;
         $user = $list["user"];
         $list = $list["list"];
         foreach ($list as $item) {
             $item_name = $item;
             if (array_key_exists($item_name, $merged)) {
+                if (in_array($user, $merged[$item_name])) continue;
                 array_push($merged[$item_name], $user);
             } else {
                 $merged[$item_name] = [$user];
             }
         }
     }
+    ksort($merged);
     return $merged;
 }
 
-function validateEmail(string $email, $regex = false)
+function validateEmail(string $email, $regex = null)
 {
-    $pattern = "/^[a-zA-Z0-9+_\-]+(\.[a-zA-Z0-9+_\-]+)*@[a-zA-Z0-9]+[a-zA-Z0-9\.\-]*[a-zA-Z0-9]+$/";
-    $result = preg_match($pattern, $email);
-    if ($regex) {
-        $result = $result && preg_match($email, $regex);
+    $pattern = "/^[a-zA-Z0-9+_\-]+(\.[a-zA-Z0-9+_\-]+)*@[a-zA-Z0-9]+[a-zA-Z0-9\.\-]+[a-zA-Z0-9]+$/";
+    $result = !!preg_match($pattern, $email);
+    if ($regex !== null) {
+        $result &= !!preg_match($regex, $email);
     }
     return !!$result;
 }
--- public/hw4/hw4_testing.php
+++ public/hw4/hw4_testing.php
@@ -1,30 +1,47 @@
 <?php
 include("homework4.php");
-?>
-<!DOCTYPE html>
-<html>
+header("Content-Type: application/json");
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="author" content="your name">
-    <meta name="description" content="include some description about your page">
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-    <title>Homework 4 Test File</title>
-</head>
+// problem 1
+$test1 = [["score" => 55, "max_points" => 100], ["score" => 55, "max_points" => 100]];
 
-<body>
-    <h1>Homework 4 Test File</h1>
+// problem 2
+$list1 = [
+    "user" => "Fred",
+    "list" => ["frozen pizza", "bread", "apples", "oranges"]
+];
+$list2 = [
+    "user" => "Wilma",
+    "list" => ["bread", "apples", "coffee"]
+];
+$list3 = [
+    "user" => "Fred",
+    "list" => ["bread", "apples", "coffee", "milk"]
+];
 
-    <h2>Problem 1</h2>
-    <?php
-    echo "Write tests for Problem 1 here\n";
-    $test1 = [["score" => 55, "max_points" => 100], ["score" => 55, "max_points" => 100]];
-    echo calculateAverage($test1, false); // should be 55
-    //...
-    ?>
+$output = [
+    "problem1" => calculateGrade($test1, false),
+    "problem2.0" => gridCorners(0, 0),
+    "problem2.1" => gridCorners(1, 1),
+    "problem2.2" => gridCorners(1, 10),
+    "problem2.3" => gridCorners(5, 1),
+    "problem3.1" => combineShoppingLists($list1, $list2),
+    "problem3.1.5" => combineShoppingLists($list1, $list2, $list3),
+    "problem3.2" => combineShoppingLists(),
+    "problem4.1" => validateEmail("orange@blue.com", "/^[a-z\.@]+$/"), // returns true
+    "problem4.2" => validateEmail("orangeblue.com", "/^[a-z\.@]+$/"), // returns false (but matches this regex)
+    "problem4.3" => validateEmail("orange123@blue.com", "/^[a-z\.@]+$/"), // returns false
+    "problem4.4" => validateEmail("mst3k@virginia.edu", "/^[a-z][a-z][a-z]?[0-9][a-z][a-z]?[a-z]?@virginia.edu$/"), // returns true
+    "problem4.5" => validateEmail("orange@virginia.edu", "/^[a-z][a-z][a-z]?[0-9][a-z][a-z]?[a-z]?@virginia.edu$/"), // returns false
+    "problem4.6" => validateEmail("orange@blue.com", "/^[a-z\.@]+$/"), // returns true
+    "problem4.7" => validateEmail("orangeblue.com", "/^[a-z\.@]+$/"), // returns false (but matches this regex)
+    "problem4.8" => validateEmail("orange123@blue.com", "/^[a-z\.@]+$/") // returns false
 
-    <p>...</p>
-</body>
 
-</html>
+];
+
+echo json_encode($output);
