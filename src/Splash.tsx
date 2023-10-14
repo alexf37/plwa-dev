1758a921082cfef777a58d726b599da517f45a1b
Alex Foster <xrk4np@virginia.edu>
2023-10-13 23:24:11 -0400

    fix call stack issue again

--- src/Splash.tsx
+++ src/Splash.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useRef } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import { Map } from "./components/Map";
-import { useMap, type MapRef, MapProvider } from "react-map-gl";
+import { type MapRef } from "react-map-gl";
 
 const INITIAL_VIEWPORT = {
   latitude: 60.035629,
@@ -8,9 +8,17 @@ const INITIAL_VIEWPORT = {
   zoom: 2.5,
 };
 
-function Helper() {
-  const { splashmap: map } = useMap();
+export function Splash() {
+  const mapRef = useRef<MapRef>(null);
+
   useEffect(() => {
+    mapRef.current?.flyTo({
+      center: [INITIAL_VIEWPORT.longitude, INITIAL_VIEWPORT.latitude],
+    });
+  }, []);
+
+  const onMapLoad = useCallback(() => {
+    const map = mapRef.current;
     if (map) {
       map.on("moveend", () => {
         const center = map.getCenter();
@@ -25,55 +33,37 @@ function Helper() {
       });
     }
   }, []);
-  return (
-    <>
-      <div></div>
-    </>
-  );
-}
-
-export function Splash() {
-  const mapRef = useRef<MapRef>(null);
-
-  useEffect(() => {
-    mapRef.current?.flyTo({
-      center: [INITIAL_VIEWPORT.longitude, INITIAL_VIEWPORT.latitude],
-    });
-  }, []);
 
   return (
     <>
-      <MapProvider>
-        <div className="pointer-events-auto fixed z-50 grid h-full w-full grid-rows-6 place-content-center">
-          <hgroup className=" row-span-3 row-start-3 flex flex-col items-center gap-2">
-            <h1 className="text-8xl font-bold text-white drop-shadow-lg">
-              <span className="text-blue-400">Spot</span>Chat
-            </h1>
-            <h2 className="text-3xl text-white">Find your spot.</h2>
-            <a
-              href="/xrk4np/app"
-              className="text-md mt-2 rounded-full border-2 border-white px-3.5 py-2.5 font-semibold text-white drop-shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-400"
-            >
-              Try it out
-            </a>
-          </hgroup>
-        </div>
-        <div className="pointer-events-none relative z-0 h-full">
-          <Map
-            ref={mapRef}
-            pitch={65}
-            initialViewState={INITIAL_VIEWPORT}
-            padding={{ top: 350, bottom: 0, left: 0, right: 0 }}
-            fog={{
-              "horizon-blend": 0.1,
-              "star-intensity": 0.6,
-            }}
-            id="splashmap"
+      <div className="pointer-events-auto fixed z-50 grid h-full w-full grid-rows-6 place-content-center">
+        <hgroup className=" row-span-3 row-start-3 flex flex-col items-center gap-2">
+          <h1 className="text-8xl font-bold text-white drop-shadow-lg">
+            <span className="text-blue-400">Spot</span>Chat
+          </h1>
+          <h2 className="text-3xl text-white">Find your spot.</h2>
+          <a
+            href="/xrk4np/app"
+            className="text-md mt-2 rounded-full border-2 border-white px-3.5 py-2.5 font-semibold text-white drop-shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-400"
           >
-            <Helper />
-          </Map>
-        </div>
-      </MapProvider>
+            Try it out
+          </a>
+        </hgroup>
+      </div>
+      <div className="pointer-events-none relative z-0 h-full">
+        <Map
+          ref={mapRef}
+          pitch={65}
+          initialViewState={INITIAL_VIEWPORT}
+          padding={{ top: 350, bottom: 0, left: 0, right: 0 }}
+          fog={{
+            "horizon-blend": 0.1,
+            "star-intensity": 0.6,
+          }}
+          id="splashmap"
+          onLoad={onMapLoad}
+        ></Map>
+      </div>
     </>
   );
 }
