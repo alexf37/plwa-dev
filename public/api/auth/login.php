883346769fdf0863679bf9ef5b2ffe79f56d4f9c
Alex Foster <xrk4np@virginia.edu>
2023-10-14 20:27:28 -0400

    remove password field from session user object

--- public/api/auth/login.php
+++ public/api/auth/login.php
@@ -14,10 +14,14 @@ handle_http_methods(function () {
         global $dbHandle;
         $result = pg_query_params($dbHandle, "SELECT * FROM users WHERE username=$1 AND password=$2;", array($username, $password));
         if (!$result) respond_server_error(500, "An error occurred querying the database.");
+
         $result = pg_fetch_all($result, PGSQL_ASSOC);
         if (count($result) === 0) respond_client_error(401, "Invalid username or password");
+
         session_start();
-        $_SESSION["user"] = $result[0];
+        $user = $result[0];
+        unset($user["password"]);
+        $_SESSION["user"] = $user;
         respond_with_success(array("success" => "Logged in successfully"));
     });
 });
