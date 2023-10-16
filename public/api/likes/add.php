5f30204c6e35985905e9d3a1ae76a79a5ab3bd92
Alex Foster <xrk4np@virginia.edu>
2023-10-16 13:48:58 -0400

    implement add/remove like endpoints

---
+++ public/api/likes/add.php
@@ -0,0 +1,28 @@
+<?php
+require("../../api-handler.php");
+require("../../postgres.php");
+
+// init db and tables
+$dbHandle = db_connect();
+if (!$dbHandle) respond_server_error(500, "An error occurred connecting to the database");
+$create_tables_result = db_init_tables($dbHandle);
+if (!$create_tables_result) respond_server_error(500, "An error occurred creating tables");
+
+//handle requests
+handle_http_methods(function () {
+    POST(["postId", "time"], function ($postId, $time) {
+        global $dbHandle;
+        session_start();
+        $user = $_SESSION["user"];
+        if (!$user) {
+            $_SESSION = array();
+            session_destroy();
+            respond_client_error(401, "Not logged in.");
+        }
+        $author = $user["id"];
+
+        $result = pg_query_params($dbHandle, "INSERT INTO likes (time, author, post) VALUES ($1, $2, $3) ON CONFLICT (author, post) DO NOTHING;", array($time, $author, $postId));
+        if (!$result) respond_server_error(500, "An error occurred liking the post");
+        respond_with_success(array("success" => "Liked post"));
+    });
+});
