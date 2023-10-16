d467f2e3dc36881cdaf6f0e8f46c78217703476a
Alex Foster <xrk4np@virginia.edu>
2023-10-16 14:04:18 -0400

    add table params to reset-db endpoint

--- public/api/reset-db.php
+++ public/api/reset-db.php
@@ -8,14 +8,29 @@ if (!$dbHandle) respond_server_error(500, "An error occurred connecting to the d
 
 // handle requests
 handle_http_methods(function () {
-    GET([], function () {
+    GET(["table?"], function ($table) {
         global $dbHandle;
-        $result = pg_query(
-            $dbHandle,
-            "DROP TABLE IF EXISTS posts CASCADE;
-            DROP TABLE IF EXISTS users CASCADE;
-            DROP TABLE IF EXISTS likes CASCADE;"
-        );
+        switch ($table) {
+            case (null):
+                $result = pg_query(
+                    $dbHandle,
+                    "DROP TABLE IF EXISTS posts CASCADE;
+                    DROP TABLE IF EXISTS users CASCADE;
+                    DROP TABLE IF EXISTS likes CASCADE;"
+                );
+                break;
+            case ("posts"):
+                $result = pg_query($dbHandle, "DROP TABLE IF EXISTS posts CASCADE;");
+                break;
+            case ("users"):
+                $result = pg_query($dbHandle, "DROP TABLE IF EXISTS users CASCADE;");
+                break;
+            case ("likes"):
+                $result = pg_query($dbHandle, "DROP TABLE IF EXISTS likes CASCADE;");
+                break;
+            default:
+                respond_client_error(400, "Invalid table name");
+        }
         if (!$result) respond_server_error(500, "An error occurred resetting the database.");
         respond_with_success(["success" => "Database reset successfully"]);
     });
