4be1a898833e28f8777325b46f77f07b4da4d653
Alex Foster <xrk4np@virginia.edu>
2023-11-01 13:24:36 -0400

    refactor req handler & support for form POSTs

--- public/api-handler.php
+++ public/api-handler.php
@@ -1,31 +1,26 @@
 <?php
-function respond(int $status_code, array $payload)
-{
+function respond(int $status_code, array $payload) {
     header("Content-Type: application/json");
     http_response_code($status_code);
     echo json_encode($payload);
     exit();
 }
 
-function respond_server_error(int $status_code = 500, string $message = "Internal server error.")
-{
+function respond_server_error(int $status_code = 500, string $message = "Internal server error.") {
     $payload = array("error" => $message);
     respond($status_code, $payload);
 }
 
-function respond_client_error(int $status_code = 400, string $message = "Bad request.")
-{
+function respond_client_error(int $status_code = 400, string $message = "Bad request.") {
     $payload = array("error" => $message);
     respond($status_code, $payload);
 }
 
-function respond_with_success(array $payload = array("success" => "Success."))
-{
+function respond_with_success(array $payload = array("success" => "Success.")) {
     respond(200, $payload);
 }
 
-function execute_with_url_params(string $method, array $method_superglobal, array $url_param_keys, callable $callback)
-{
+function execute_with_input(string $method, array $method_superglobal, array $url_param_keys, callable $callback) {
     if ($_SERVER['REQUEST_METHOD'] == $method) {
         $args = array();
         foreach ($url_param_keys as $key => $value) {
@@ -43,27 +38,25 @@ function execute_with_url_params(string $method, array $method_superglobal, arra
 }
 
 // callback must execute a respond function
-function GET(array $url_param_keys, callable $callback)
-{
-    execute_with_url_params("GET", $_GET, $url_param_keys, $callback);
+function GET(array $url_param_keys, callable $callback) {
+    execute_with_input("GET", $_GET, $url_param_keys, $callback);
 }
 // callback must execute a respond function
-function POST(array $url_param_keys, callable $callback)
-{
+function POST(array $url_param_keys, callable $callback) {
     $request_body = json_decode(file_get_contents('php://input'), true);
-    if ($request_body === null) respond_client_error(400, "Invalid request body.");
-    execute_with_url_params("POST", $request_body, $url_param_keys, $callback);
+    if ($request_body === null)
+        respond_client_error(400, "Invalid request body.");
+    execute_with_input("POST", $request_body, $url_param_keys, $callback);
 }
 
-function PATCH(array $url_param_keys, callable $callback)
-{
+function PATCH(array $url_param_keys, callable $callback) {
     $request_body = json_decode(file_get_contents('php://input'), true);
-    if ($request_body === null) respond_client_error(400, "Invalid request body.");
-    execute_with_url_params("PATCH", $request_body, $url_param_keys, $callback);
+    if ($request_body === null)
+        respond_client_error(400, "Invalid request body.");
+    execute_with_input("PATCH", $request_body, $url_param_keys, $callback);
 }
 
-function handle_http_methods(callable $handler)
-{
+function handle_http_methods(callable $handler) {
     try {
         $handler();
     } catch (Exception $e) {
